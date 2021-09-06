74889de69c7a2a6e56fa91ef60932734c98e077f
Wiktor Pacer <kontakt@pacerit.pl>
2021-09-06 20:09:44 +0200

    passport_number rule
    - add passport_number validation rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -2,11 +2,12 @@
 
 return [
 
-    'PESEL'          => 'PESEL number is not valid!',
-    'REGON'          => 'REGON number is not valid!',
-    'NIP'            => 'NIP number is not valid!',
-    'id_card_number' => 'ID Card number is not valid!',
-    'post_code'      => 'Post code not valid!',
-    'PWZ'            => 'PWZ code not valid!',
+    'PESEL'           => 'PESEL number is not valid!',
+    'REGON'           => 'REGON number is not valid!',
+    'NIP'             => 'NIP number is not valid!',
+    'id_card_number'  => 'ID Card number is not valid!',
+    'post_code'       => 'Post code not valid!',
+    'PWZ'             => 'PWZ code not valid!',
+    'passport_number' => 'Passport number is not valid!',
 
 ];
--- resources/lang/pl/validation.php
+++ resources/lang/pl/validation.php
@@ -2,11 +2,12 @@
 
 return [
 
-    'PESEL'          => 'Numer PESEL jest niepoprawny!',
-    'REGON'          => 'Numer REGON jest niepoprawny',
-    'NIP'            => 'Numer NIP jest niepoprawny!',
-    'id_card_number' => 'Numer dowodu osobistego jest niepoprawny!',
-    'post_code'      => 'Kod pocztowy niepoprawny!',
-    'PWZ'            => 'Numer PWZ niepoprawny!',
+    'PESEL'           => 'Numer PESEL jest niepoprawny!',
+    'REGON'           => 'Numer REGON jest niepoprawny',
+    'NIP'             => 'Numer NIP jest niepoprawny!',
+    'id_card_number'  => 'Numer dowodu osobistego jest niepoprawny!',
+    'post_code'       => 'Kod pocztowy niepoprawny!',
+    'PWZ'             => 'Numer PWZ niepoprawny!',
+    'passport_number' => 'Numer paszportu jest niepoprawny!',
 
 ];
