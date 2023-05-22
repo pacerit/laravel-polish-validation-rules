51c9f55d0ef68579491643c64c329fe2bf7cf4e2
Wiktor Pacer <53344063+WiktorPacer@users.noreply.github.com>
2023-05-22 21:10:39 +0200

    Issue #14 (#15)
    - update 'regon' validation rule - support validation 14 digit number

--- tests/Unit/REGONRuleTest.php
+++ tests/Unit/REGONRuleTest.php
@@ -35,7 +35,32 @@ class REGONRuleTest extends AbstractRuleTestCase
      */
     public function testValidREGON()
     {
-        $this->assertEquals(true, $this->rule->passes('regon', '499273139'));
+        $validNumbers = [
+            '734530267',
+            '053955500',
+            '253338390',
+            '947187850',
+            '130433149',
+            '843308426',
+            '386679662',
+            '347116739',
+            '988319794',
+            '303398620',
+            '75147943205580',
+            '25713726382524',
+            '48323221384555',
+            '30936418398037',
+            '63946570931273',
+            '45829482847518',
+            '27025051044606',
+            '94035209178263',
+            '20978490719275',
+            '08942642204556',
+        ];
+
+        foreach ($validNumbers as $number) {
+            $this->assertTrue($this->rule->passes('regon', $number));
+        }
     }
 
     /**
@@ -47,7 +72,10 @@ class REGONRuleTest extends AbstractRuleTestCase
      */
     public function testNotValidREGON()
     {
-        $this->assertEquals(false, $this->rule->passes('regon', '499273138'));
+        $this->assertFalse($this->rule->passes('regon', '499273138'));
+        $this->assertFalse($this->rule->passes('regon', '490003138'));
+        $this->assertFalse($this->rule->passes('regon', '574625967547362'));
+        $this->assertFalse($this->rule->passes('regon', '653231231237362'));
     }
 
     /**
